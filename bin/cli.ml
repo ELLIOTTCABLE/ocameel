3f523b37974a987d37557dedc28de58bcc7ee1eb
ELLIOTTCABLE <me@ell.io>
2017-10-03 15:13:41 -0500

    + more succinct

--- bin/cli.ml
+++ bin/cli.ml
@@ -1,23 +1,17 @@
 open Core
 
 
-let parse filename =
+let parse filename () =
    Ocameel.read_source_from filename |> Ocameel.print_source
 
 
-let spec =
-  let open Command.Spec in
-  empty
-  +> anon ("filename" %: string)
-
 let command =
-  Command.basic
-    ~summary:"Run some Scheme code"
-    spec
-    (fun filename () ->
-       parse filename)
+   Command.basic
+      ~summary:"Run some Scheme code"
+      Command.Spec.(empty +> anon ("source file" %: file))
+      parse
 
 let () =
-  Exn.handle_uncaught ~exit:true (fun () ->
-     Command.run ~version:"1.0" ~build_info:"RWO" command
-  )
+   Exn.handle_uncaught ~exit:true (fun () ->
+      Command.run ~version:"1.0" ~build_info:"RWO" command
+   )
