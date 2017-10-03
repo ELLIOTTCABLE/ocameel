9af873b2bc38aaa9e746689c0c1994ae7b9080ad
ELLIOTTCABLE <me@ell.io>
2017-10-03 16:38:28 -0500

    + ocp-indent!

--- bin/cli.ml
+++ bin/cli.ml
@@ -3,9 +3,9 @@ open Core
 
 let do_parse files =
    List.iter files (fun file -> (match file with
-      | "-"       -> Ocameel.input_source In_channel.stdin
-      | filename  -> Ocameel.load_source filename
-   ) |> Ocameel.print_source)
+         | "-"       -> Ocameel.input_source In_channel.stdin
+         | filename  -> Ocameel.load_source filename
+      ) |> Ocameel.print_source)
 
 
 let command =
@@ -16,12 +16,12 @@ let command =
          +> anon (sequence ("filename" %: file))
       )
       (fun files () ->
-         match files with
-         | []     -> do_parse ["-"]
-         | files  -> do_parse files
+          match files with
+          | []     -> do_parse ["-"]
+          | files  -> do_parse files
       )
 
 let () =
    Exn.handle_uncaught ~exit:true (fun () ->
-      Command.run ~version:"0.1" command
-   )
+         Command.run ~version:"0.1" command
+      )
--- lib/ocameel.ml
+++ lib/ocameel.ml
@@ -8,10 +8,10 @@ let load_source path =
 
 let rec print_source ?(channel = stdout) sexps =
    List.iter sexps (fun sexp ->
-      Sexp.output_hum channel sexp ;
-      Out_channel.newline stdout
-   )
+         Sexp.output_hum channel sexp ;
+         Out_channel.newline stdout
+      )
 
 (* FIXME: No idea why this refuses to work. *)
 (* let formatter = Format.formatter_of_out_channel channel in
-   Sexp.pp_hum formatter |> List.iter sexps *)
+      Sexp.pp_hum formatter |> List.iter sexps *)
