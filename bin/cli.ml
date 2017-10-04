cf6119408a4dec6ffbee6924f8c95bb3626b7772
ELLIOTTCABLE <me@ell.io>
2017-10-04 10:15:14 -0500

    + adding output-to-file

--- bin/cli.ml
+++ bin/cli.ml
@@ -3,53 +3,71 @@ open Core
 
 (* ### ocameel parse ### *)
 
-let do_parse files =
+let do_parse files output =
+   let out_channel = match output with
+   | "-"       -> stdout
+   | filename  -> Out_channel.create filename
+   in
    List.iter files (fun file -> (match file with
          | "-"       -> Ocameel.input_source In_channel.stdin
          | filename  -> Ocameel.load_source filename
-      ) |> Ocameel.print_source)
+      ) |> Ocameel.print_source ~channel:out_channel)
 
-let parse =
+let parse ~common =
    Command.basic
       ~summary:"Output a stripped-and-formatted version of a provided Scheme file"
       Command.Spec.(
          empty
          +> anon (sequence ("filename" %: file))
+         ++ common
       )
-      (fun files () ->
+      (fun files output () ->
           match files with
-          | []     -> do_parse ["-"]
-          | files  -> do_parse files
+          | []     -> do_parse ["-"] output
+          | files  -> do_parse files output
       )
 
 
 (* ### ocameel compile ### *)
 
-let do_compile files =
+let do_compile files output =
+   let out_channel = match output with
+   | "-"       -> stdout
+   | filename  -> Out_channel.create filename
+   in
    List.iter files (fun file ->
          let source = (match file with
                | "-"       -> Ocameel.input_source In_channel.stdin
                | filename  -> Ocameel.load_source filename
             ) in
-         Ocameel.compile_program source stdout)
+         Ocameel.compile_program source out_channel)
 
-let compile =
+let compile ~common =
    Command.basic
       ~summary:"Compile some Scheme code to x86 assembly"
       Command.Spec.(
          empty
          +> anon (sequence ("filename" %: file))
+         ++ common
       )
-      (fun files () ->
+      (fun files output () ->
           match files with
-          | []     -> do_compile ["-"]
-          | files  -> do_compile files
+          | []     -> do_compile ["-"] output
+          | files  -> do_compile files output
       )
 
 
 let () =
+   let common =
+    Command.Spec.(
+      empty
+      +> flag "-o" (optional_with_default "-" file) ~doc:" Output filename"
+   (* +> flag "-d" (optional_with_default false bool) ~doc:" Debug mode" *)
+   (* +> flag "-v" (optional_with_default false bool) ~doc:" Verbose output" *)
+    )
+  in
    Exn.handle_uncaught ~exit:true (fun () ->
          Command.group ~summary:"Interact with Scheme code"
-            [ "parse", parse; "compile", compile ]
+            [ "parse", parse common; "compile", compile common ]
          |> Command.run ~version:"0.1"
       )
