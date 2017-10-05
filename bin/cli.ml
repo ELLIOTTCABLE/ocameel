0108a2210160e0ce13695074c2b4dfdced1bc69e
ELLIOTTCABLE <me@ell.io>
2017-10-05 11:29:05 -0500

    + replacing my paws.js-ish CLI with a gcc/clang-style one

--- bin/cli.ml
+++ bin/cli.ml
@@ -1,73 +1,83 @@
 open Core
 
+type stage = Parsing | Compilation | Assembly | Linking
 
-(* ### ocameel parse ### *)
 
 let do_parse files output =
    let out_channel = match output with
-   | "-"       -> stdout
-   | filename  -> Out_channel.create filename
+      | "-"       -> stdout
+      | filename  -> Out_channel.create (filename ^ ".scm")
    in
    List.iter files (fun file -> (match file with
          | "-"       -> Ocameel.input_source In_channel.stdin
-         | filename  -> Ocameel.load_source filename
-      ) |> Ocameel.print_source ~channel:out_channel)
-
-let parse ~common =
-   Command.basic
-      ~summary:"Output a stripped-and-formatted version of a provided Scheme file"
-      Command.Spec.(
-         empty
-         +> anon (sequence ("filename" %: file))
-         ++ common
-      )
-      (fun files output () ->
-          match files with
-          | []     -> do_parse ["-"] output
-          | files  -> do_parse files output
-      )
-
-
-(* ### ocameel compile ### *)
+         | filename  -> Ocameel.load_source filename )
+                                |> Ocameel.print_source ~channel:out_channel )
 
 let do_compile files output =
    let out_channel = match output with
-   | "-"       -> stdout
-   | filename  -> Out_channel.create filename
+      | "-"       -> stdout
+      | filename  -> Out_channel.create filename
    in
    List.iter files (fun file ->
          let source = (match file with
                | "-"       -> Ocameel.input_source In_channel.stdin
-               | filename  -> Ocameel.load_source filename
-            ) in
-         Ocameel.compile_program source out_channel)
+               | filename  -> Ocameel.load_source filename)
+         in
+         Ocameel.compile_program source out_channel )
 
-let compile ~common =
+(* --------------------------- *)
+
+let do_stages stage files output =
+   match stage with
+   | Parsing      -> do_parse files output
+   | Compilation  -> do_compile files output
+   | Assembly     -> failwith "NYI: invoking assembler"
+   | Linking      -> failwith "NYI: invoking linker"
+
+let command =
+   let spec = Command.Spec.(
+         empty
+         +> flag "-E"
+               no_arg ~doc:"boolean Only invoke the parser (effectively pretty-printing and \
+                            concatenating the input files)"
+
+         +> flag "-S"
+               no_arg ~doc:"boolean Only compile an assembly file (overrides -E)"
+
+         +> flag "-c"
+               no_arg ~doc:"boolean Compile, then invoke the assembler; generating a target .o \
+                            object-file (overrides -E, -S)"
+
+         +> flag "-o"
+               (optional file) ~doc:" Output filename (will be appended with an appropriate \
+                                     suffix, based on the chosen stage)"
+
+         (* +> flag "-d" (optional_with_default false bool) ~doc:" Debug mode" *)
+         (* +> flag "-v" (optional_with_default false bool) ~doc:" Verbose output" *)
+         +> anon (sequence ("filename" %: file)) )
+   in
+
    Command.basic
       ~summary:"Compile some Scheme code to x86 assembly"
-      Command.Spec.(
-         empty
-         +> anon (sequence ("filename" %: file))
-         ++ common
-      )
-      (fun files output () ->
-          match files with
-          | []     -> do_compile ["-"] output
-          | files  -> do_compile files output
-      )
+      ~readme:(fun () ->
+            "By default, the compiler invokes both the assembler and the linker, producing an
+executable. This can be overriden by selecting lesser stages with `-E`, `-S`,
+or `-c`." )
 
+      spec
+
+      (fun parse_only and_generate and_invoke_assembler output files  () ->
+          let output = match output with | None -> "a" | Some filename -> filename in
+          let selected_stage =
+             if (and_invoke_assembler) then Assembly
+             else if (and_generate) then Compilation
+             else if (parse_only) then Parsing
+             else Linking
+          in
+          match files with
+          | []     -> do_stages selected_stage ["-"] output
+          | files  -> do_stages selected_stage files output )
 
 let () =
-   let common =
-    Command.Spec.(
-      empty
-      +> flag "-o" (optional_with_default "-" file) ~doc:" Output filename"
-   (* +> flag "-d" (optional_with_default false bool) ~doc:" Debug mode" *)
-   (* +> flag "-v" (optional_with_default false bool) ~doc:" Verbose output" *)
-    )
-  in
    Exn.handle_uncaught ~exit:true (fun () ->
-         Command.group ~summary:"Interact with Scheme code"
-            [ "parse", parse common; "compile", compile common ]
-         |> Command.run ~version:"0.1"
-      )
+         Command.run ~version:"0.1" command )
